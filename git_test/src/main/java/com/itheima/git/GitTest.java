5206f48851f7dc67c34648175e606054c8022a9a
tlg1471071423 <1471071423@qq.com>
2018-11-18 21:20:37 +0800

    第一次添加

---
+++ git_test/src/main/java/com/itheima/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+/**
+ * @author tlg
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("豹女");
+        System.out.println("男枪");
+        System.out.println("螳螂");
+    }
+}
